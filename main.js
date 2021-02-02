91de4397ccb63b67fad8415ded2c283407b96d32
fatondomi <fatonidomi@gmail.com>
2021-02-03 00:58:51 +0100

    new files

---
+++ main.js
@@ -0,0 +1,18 @@
+const electron=require("electron");
+const {app, BrowserWindow}=electron;
+
+//per mi hjek security warnings
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = true;
+
+app.on('ready',()=>{
+	let win = new BrowserWindow(
+	{
+		height:800,
+		width:1200,
+		frame:true,
+		resizable:true,
+	});
+	//win.setMenu(null);
+	win.loadURL("file:///./resources/ui.html");
+	win.webContents.openDevTools();
+});
