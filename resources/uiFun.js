1d94c37175f34eb47eb581b82cf8ab12172785ba
fatondomi <fatondomi@yahoo.com>
2021-02-04 08:59:26 +0100

    bug fixes

--- resources/uiFun.js
+++ resources/uiFun.js
@@ -1,33 +1,33 @@
 //ui functions
 
 let tables = ["familjet","aktivitetet"];
-const navBar = document.getElementById("navbar");
 
 function populateNavbarWithTables()
 {
-    let newLi;
-    let newA;
+    let navBar = document.getElementById("navbar");
 
-    for(tableName in tables)
+    for(tableIndex in tables)
     {
+        let newLi;
+        let newA;
+
         newLi = document.createElement("li");
-        newLi.classList.add("nav-item active");
+        newLi.classList.add("nav-item");
+        newLi.classList.add("active");
+        newLi.style.cursor = "pointer";
 
         newA = document.createElement("a");
         newA.classList.add("nav-link");
-        newA.appendChild(document.createTextNode(tableName));
-        newA.onclick = tableClicked();
+        newA.appendChild(document.createTextNode(tables[tableIndex]));
+        newA.onclick = tableClicked;
 
         newLi.appendChild(newA);
-        navBar.appendChid(newLi)
-        console.log("u krijua")
+        navBar.appendChild(newLi)
+        console.log("u krijua "+tables[tableIndex])
     }
 }
 
-function tableClicked(e)
+function tableClicked()
 {
-    console.log("table clicked was "+e.target.innerText);
+    console.log("table clicked");
 }
-
-
-document.addEventListener("DOMContentLoaded",(e)=>{populateNavbarWithTables();})
